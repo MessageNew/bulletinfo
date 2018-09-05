4bce587f5461ec78fedfac164447d2cf1dc9d7c1
MysteriousBooks <38039511+MysteriousBooks@users.noreply.github.com>
2018-09-05 15:22:29 +0800

    更新

---
+++ src/main/java/com/bulletinfo/www/domain/User.java
@@ -0,0 +1,105 @@
+package com.bulletinfo.www.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * Created by Mysteriouseyes on 2018/9/5.
+ */
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private Integer Uid;
+    private String Username;
+    private String time;
+    private String password;
+    private String Sex;
+    private String phone;
+    private String age;
+    private String address;
+    private String icourl;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUid() {
+        return Uid;
+    }
+
+    public void setUid(Integer uid) {
+        Uid = uid;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSex() {
+        return Sex;
+    }
+
+    public void setSex(String sex) {
+        Sex = sex;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getIcourl() {
+        return icourl;
+    }
+
+    public void setIcourl(String icourl) {
+        this.icourl = icourl;
+    }
+}
---
+++ src/main/java/com/bulletinfo/www/respository/UserRespository.java
@@ -0,0 +1,11 @@
+package com.bulletinfo.www.respository;
+
+import com.bulletinfo.www.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by Mysteriouseyes on 2018/9/5.
+ */
+public interface UserRespository extends JpaRepository<User, Integer> {
+
+}
