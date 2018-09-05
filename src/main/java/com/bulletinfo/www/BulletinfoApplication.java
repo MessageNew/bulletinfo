92c1235a97f1d89e5c3a10c05b9ad96298bc1219
MysteriousBooks <38039511+MysteriousBooks@users.noreply.github.com>
2018-09-05 14:18:18 +0800

    Initial commit

---
+++ src/main/java/com/bulletinfo/www/BulletinfoApplication.java
@@ -0,0 +1,12 @@
+package com.bulletinfo.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulletinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulletinfoApplication.class, args);
+	}
+}
